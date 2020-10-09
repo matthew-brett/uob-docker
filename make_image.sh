9628bc6b5c7a29390bd2d7f03b52945f02ff6edd
Matthew Brett <matthew.brett@gmail.com>
2020-10-09 21:57:41 +0100

    Generalize build script

--- make_image.sh
+++ make_image.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+pth=$1
+if [ -z "$pth" ]; then
+    echo Specify path to build
+    exit 1
+fi
+if [[ -n $(git status --short) ]]; then
+    git status
+    echo Clean git status and commit before building image.
+    exit 2
+fi
+
 tag=$(git rev-parse --short HEAD)
 name=$(basename $PWD)
 user=gcr.io/uob-jupyterhub
 img_name="${name}:${tag}"
 full_name="${user}/${img_name}"
-docker build -t ${full_name} .
+docker build -t ${full_name} ${pth}
 docker push ${full_name}
