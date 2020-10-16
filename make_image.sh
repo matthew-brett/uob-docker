e8bab81d79fe1887352894b4590527e162220fe1
Matthew Brett <matthew.brett@gmail.com>
2020-10-16 15:22:33 +0100

    Use EU to house images

--- make_image.sh
+++ make_image.sh
@@ -13,7 +13,7 @@ fi
 gcloud auth configure-docker
 tag=$(git rev-parse --short HEAD)
 name=$(basename $PWD)
-user=gcr.io/uob-jupyterhub
+user=eu.gcr.io/uob-jupyterhub
 img_name="${name}:${tag}"
 full_name="${user}/${img_name}"
 docker build -t ${full_name} ${pth}
